263d0122fdb4339df141eb805f9963b181c3d5f3
Harekrishna Nayak <harekrishnanayak1997@gmail.com>
2023-10-17 21:44:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('item');
 
+form.addEventListener('submit', addItem);
+
+function addItem(e){
+    e.preventDefault();
+
+    var newItem = document.getElementById('item');
+
+    var li = document.createElement('li');
+
+    var console: 'list-group-item';
+}
