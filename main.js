84d1c00d56405cb8bf2d1392e4624ccf3f5702f9
Harekrishna Nayak <harekrishnanayak1997@gmail.com>
2023-10-17 22:07:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('item');
+var filter = document.getElementById('filter');
 
 form.addEventListener('submit', addItem);
 
@@ -11,4 +12,25 @@ function addItem(e){
     var li = document.createElement('li');
 
     var console: 'list-group-item';
+}
+
+//filter event
+filter.addEventListener('keyup', filterItems);
+
+
+
+//filer Items
+function filterItems(e){
+    var text =e.target.value.toLowerCase();
+
+    var items = items = itemList.getElementsByTagName('li');
+
+    Array.form(items).forEach(function(item){
+      var itemName = item.firstchild.textContent;  
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      }else{
+       item.style.display = 'none' ;
+      }
+    });
 }
